36013b157c23f1f21fa2246fbb5200d9871572ef
Junior Conquista <juniorconquista@Administradors-MacBook-Pro.local>
2024-03-25 14:51:09 -0300

    chore: adjusts storybook

--- packages/implementations/vuejs/src/atomic/Button/Button.vue
+++ packages/implementations/vuejs/src/atomic/Button/Button.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     :class="button.classnames.button({ size, variant, appearance })"
-    v-bind="props"
+    :bind="$props"
   >
     {{ children }}
     <span :class="button.classnames.overlay" />
@@ -10,88 +10,46 @@
 
 <script lang="ts" setup>
 import { button } from "@rarui/styles/src";
-// import type { ButtonHTMLAttributes } from "vue";
+import type { ButtonHTMLAttributes, Component } from "vue";
 
-// interface Props extends Partial<ButtonHTMLAttributes> {
-// interface ButtonProps {
-//   /**
-//    * The content of the button.
-//    * @TJS-type React.ReactNode
-//    */
-//   children: any;
-//   /**
-//    * Change the visual style of the button.
-//    * @default brand
-//    */
-//   appearance?:
-//     | "brand"
-//     | "brand-secondary"
-//     | "danger"
-//     | "success"
-//     | "warning"
-//     | "neutral"
-//     | "inverted";
-//   /**
-//    * Change the size of the button.
-//    * @default large
-//    */
-//   size?: "large" | "medium" | "small";
-//   /**
-//    * Change the variant of the button.
-//    * @default solid
-//    */
-//   variant?: "solid" | "outlined" | "text" | "tonal";
-//   /**
-//    * Disables the button, disallowing user interaction.
-//    * @default false
-//    */
-//   disabled?: boolean;
-// }
+interface ButtonProps extends /* @vue-ignore */ ButtonHTMLAttributes {
+  /**
+   * The content of the button.
+   * @TJS-type Component | string
+   */
+  children: Component | string;
+  /**
+   * Change the visual style of the button.
+   * @default brand
+   */
+  appearance?:
+    | "brand"
+    | "brand-secondary"
+    | "danger"
+    | "success"
+    | "warning"
+    | "neutral"
+    | "inverted";
+  /**
+   * Change the size of the button.
+   * @default large
+   */
+  size?: "large" | "medium" | "small";
+  /**
+   * Change the variant of the button.
+   * @default solid
+   */
+  variant?: "solid" | "outlined" | "text" | "tonal";
+  /**
+   * Disables the button, disallowing user interaction.
+   * @default false
+   */
+  disabled?: boolean;
+}
 
-// withDefaults(defineProps<ButtonProps>(), {
-//   appearance: "brand",
-//   size: "large",
-//   variant: "solid",
-// });
-
-const { ...props } = withDefaults(
-  defineProps<{
-    /**
-     * The content of the button.
-     * @TJS-type React.ReactNode
-     */
-    children: any;
-    /**
-     * Change the visual style of the button.
-     * @default brand
-     */
-    appearance?:
-      | "brand"
-      | "brand-secondary"
-      | "danger"
-      | "success"
-      | "warning"
-      | "neutral"
-      | "inverted";
-    /**
-     * Change the size of the button.
-     * @default large
-     */
-    size?: "large" | "medium" | "small";
-    /**
-     * Change the variant of the button.
-     * @default solid
-     */
-    variant?: "solid" | "outlined" | "text" | "tonal";
-    /**
-     * Disables the button, disallowing user interaction.
-     * @default false
-     */
-    disabled?: boolean;
-  }>(),
-  { appearance: "brand", size: "large", variant: "solid" },
-);
-
-// console.log(buttonProps);
-// console.log(buttonProps);
+withDefaults(defineProps<ButtonProps>(), {
+  appearance: "brand",
+  size: "large",
+  variant: "solid",
+});
 </script>
