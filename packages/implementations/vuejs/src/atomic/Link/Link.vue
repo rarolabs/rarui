33463dea1941308c2d98de5df3b113a5b635f77d
Mauricio Silva <mauriciospta@gmail.com.br>
2024-03-26 16:22:25 -0300

    cria componente de link para vue

---
+++ packages/implementations/vuejs/src/atomic/Link/Link.vue
@@ -0,0 +1,28 @@
+<template>
+  <a :class="link.classnames.link({ appearance, size })" v-bind="props">
+    <slot />
+  </a>
+</template>
+
+<script lang="ts" setup>
+import { link } from "@rarui/styles/src";
+import { LinkHTMLAttributes } from "vue";
+
+export interface LinkProps extends /* @vue-ignore */ LinkHTMLAttributes {
+  /**
+   * Change the visual style of the link.
+   * @default default
+   */
+  appearance?: "default" | "neutral" | "inverted";
+  /**
+   * Change the size of the link.
+   * @default large
+   */
+  size?: "large" | "medium" | "small";
+}
+
+const { ...props } = withDefaults(defineProps<LinkProps>(), {
+  appearance: "default",
+  size: "large",
+});
+</script>
