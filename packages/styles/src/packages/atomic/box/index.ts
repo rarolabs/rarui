320942b559f7acdc04ee588794b917034e85f657
Junior Conquista <juniorconquista@Administradors-MacBook-Pro.local>
2024-04-10 14:57:47 -0300

    feat: 🎸 added new style pack for box component

---
+++ packages/styles/src/packages/atomic/box/index.ts
@@ -0,0 +1,8 @@
+import { boxSprinkle } from "./rarui-box.css";
+
+export const box = {
+  ...boxSprinkle,
+};
+
+// export type { BoxSprinkle } from "./rarui-box.types";
+export type { BoxSprinkle } from "./rarui-box.css";
