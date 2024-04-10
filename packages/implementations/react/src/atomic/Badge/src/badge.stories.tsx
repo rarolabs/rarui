fef2b5acaf2405291ddb3a514a91e34d309fc5b4
Mauricio Silva <mauriciospta@gmail.com.br>
2024-04-10 16:41:25 -0300

    refactor: 💡 alteracao docs storybook

--- packages/implementations/react/src/atomic/Badge/src/badge.stories.tsx
+++ packages/implementations/react/src/atomic/Badge/src/badge.stories.tsx
@@ -1,53 +1,33 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
+import { argTypesConvert } from ".storybook/utils";
+import docs from "./badge.docs.json";
 
-import { Badge as BadgeComponent } from "./Badge";
-import type { BadgeProps } from "./";
-
-export const Basic: React.FC<BadgeProps> = ({
-  children = "Badge",
-  ...props
-}) => <BadgeComponent {...props}>{children}</BadgeComponent>;
-
-Basic.displayName = "Badge";
+import { Badge } from "./Badge";
 
 const meta = {
   title: "Atomic/Badge",
-  component: Basic,
+  component: Badge,
   argTypes: {
+    ...argTypesConvert(docs),
     children: { control: { type: "text" } },
-    appearance: {
-      control: "select",
-      options: [
-        "brand",
-        "danger",
-        "success",
-        "warning",
-        "info",
-        "neutral",
-        "inverted",
-      ],
-    },
-    size: {
-      control: "select",
-      options: ["small", "medium", "large", "dot"],
-    },
   },
   tags: ["autodocs"],
-} satisfies Meta<typeof Basic>;
+} satisfies Meta<typeof Badge>;
 
 export default meta;
-type Story = StoryObj<typeof Basic>;
+type Story = StoryObj<typeof Badge>;
 
 export const basic: Story = {
   args: {
     appearance: "brand",
+    children: "Badge",
   },
 };
 
 export const outlined: Story = {
   args: {
-    children: "badge",
+    children: "Badge",
     variant: "outlined",
   },
 };
@@ -56,5 +36,6 @@ export const solid: Story = {
   args: {
     variant: "solid",
     appearance: "success",
+    children: "Badge",
   },
 };
