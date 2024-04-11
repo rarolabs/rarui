532441e53dc510df4d091d5526b8323f64bb9875
Junior Conquista <juniorconquista@Administradors-MacBook-Pro.local>
2024-04-11 20:23:13 -0300

    test: 💍 added new atomic Skeleton component

---
+++ packages/implementations/react/src/atomic/Skeleton/src/skeleton.spec.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import { Skeleton } from "./Skeleton";
+import { SkeletonProps } from "./skeleton.types";
+
+const makeSut = (props: SkeletonProps) => {
+  render(<Skeleton {...props} data-testid="skeleton-element" />);
+};
+
+describe("GIVEN <Skeleton />", () => {
+  describe("WHEN rendered", () => {
+    it("THEN should ensure dynamic height and width properties are applied", () => {
+      makeSut({
+        height: "10rem",
+        width: "50rem",
+      });
+      const skeleton = screen.getByTestId("skeleton-element");
+      expect(skeleton.getAttribute("style")).toMatch(
+        /--width-xs__\w{0,9}: 50rem;/,
+      );
+      expect(skeleton.getAttribute("style")).toMatch(
+        /--height-xs__\w{0,9}: 10rem;/,
+      );
+    });
+
+    it("THEN should ensure dynamic border radius properties are applied", () => {
+      makeSut({
+        height: "10rem",
+        width: "50rem",
+        borderRadius: "0.25rem",
+      });
+      const skeleton = screen.getByTestId("skeleton-element");
+      expect(skeleton.getAttribute("style")).toMatch(
+        /--borderRadius-xs__\w{0,9}: 0.25rem;/,
+      );
+    });
+  });
+});
