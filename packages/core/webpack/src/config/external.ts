9e1497ed13bf9c3b7657778ecef5bc4e10d9adb5
Junior Conquista <juniorconquista@Administradors-MacBook-Pro.local>
2024-04-23 14:49:27 -0300

    test: 💍 externalizing the package build styles package

--- packages/core/webpack/src/config/external.ts
+++ packages/core/webpack/src/config/external.ts
@@ -3,13 +3,13 @@
  * @see https://webpack.js.org/configuration/externals/
  */
 export const externalLibs = {
+  "@rarui/styles": "@rarui/styles",
   react: "react",
   "react-dom": "react-dom",
   vue: "vue",
 };
 
 export const externalPackages = {
-  "@rarui/styles": "@rarui/styles",
   "@rarui/icons": "@rarui/icons",
   "@rarui/tokens": "@rarui/tokens",
 };
