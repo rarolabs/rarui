98fad1d49f47a3edf1d88601d07deb98920ebf82
Junior Conquista <juniorconquista@Administradors-MacBook-Pro.local>
2024-04-25 11:41:34 -0300

    test: 💍 added new style pack for toggle component

--- packages/implementations/react/src/atomic/Toggle/src/toggle.spec.tsx
+++ packages/implementations/react/src/atomic/Toggle/src/toggle.spec.tsx
@@ -15,7 +15,7 @@ describe("GIVEN <Toggle />", () => {
       expect(screen.getByText("My Toggle")).toBeDefined();
     });
 
-    it("THEN should active the toggle correctly", () => {
+    it("AND should active the toggle correctly", () => {
       makeSut({ name: "toggle", label: "My Toggle" });
       expect(
         screen.getByTestId<HTMLInputElement>("toggle-input").checked,
@@ -26,11 +26,50 @@ describe("GIVEN <Toggle />", () => {
       ).toBeTruthy();
     });
 
-    it("THEN should active the toggle correctly for default", () => {
-      makeSut({ name: "toggle", active: true });
+    it("AND should selected the toggle correctly for default", () => {
+      makeSut({ name: "toggle", selected: true });
       expect(
         screen.getByTestId<HTMLInputElement>("toggle-input").checked,
       ).toBeTruthy();
+    });
+  });
+
+  describe("THEN should correctly render the submitted size", () => {
+    it("THEN should correctly render the size default", () => {
+      makeSut({ name: "toggle" });
+      expect(screen.getByTestId("slider").getAttribute("class")).toContain(
+        "size_large",
+      );
+    });
+
+    it("AND should correctly render the size large", () => {
+      makeSut({ name: "toggle", size: "large" });
+      expect(screen.getByTestId("slider").getAttribute("class")).toContain(
+        "size_large",
+      );
+    });
+
+    it("AND should correctly render the size small", () => {
+      makeSut({ name: "toggle", size: "small" });
+      expect(screen.getByTestId("slider").getAttribute("class")).toContain(
+        "size_small",
+      );
+    });
+  });
+
+  describe("THEN should correctly render the submitted error", () => {
+    it("THEN should correctly render the error false", () => {
+      makeSut({ name: "toggle" });
+      expect(screen.getByTestId("slider").getAttribute("class")).not.toContain(
+        "error_true",
+      );
+    });
+
+    it("AND should correctly render the error true", () => {
+      makeSut({ name: "toggle", error: true });
+      expect(screen.getByTestId("slider").getAttribute("class")).toContain(
+        "error_true",
+      );
     });
   });
 });
