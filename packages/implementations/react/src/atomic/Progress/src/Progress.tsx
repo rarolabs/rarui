f8e72db2a632b6c8e5d72645c6a8e126332b7c1e
Jefferson Lima <jefferson.lima@rarolabs.com.br>
2024-05-09 15:48:59 -0300

    test: added tests to atomic Progress component

--- packages/implementations/react/src/atomic/Progress/src/Progress.tsx
+++ packages/implementations/react/src/atomic/Progress/src/Progress.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { progressBar } from "@rarui/styles";
+import { progress } from "@rarui/styles";
 import { ProgressProps } from "./progress.types";
 
 const Progress: React.FC<ProgressProps> = ({
@@ -8,19 +8,22 @@ const Progress: React.FC<ProgressProps> = ({
   percentage,
   ...props
 }: ProgressProps) => {
-  const { className, style, otherProps } = progressBar.sprinkle({
+  const dataTestid = (props as { "data-testid": string })?.["data-testid"];
+
+  const { className, style, otherProps } = progress.sprinkle({
     width: `${percentage}%`
   });
   const completed: boolean = percentage >= 100;
   return (
-    <div {...props} className={progressBar.classnames.progress()}>
+    <div {...props} className={progress.classnames.progress()}>
       <div
         className={[
-          progressBar.classnames.progressBar({ completed }),
+          progress.classnames.progressBar({ completed }),
           className
         ].join(" ")}
         style={style}
         {...otherProps}
+        data-testid={dataTestid ? `${dataTestid}-bar` : ""}
       />
     </div>
   );
