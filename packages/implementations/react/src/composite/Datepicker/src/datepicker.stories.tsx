5b9b199b718a123924a1e3de9d80446b39bfc856
Junior Conquista <juniorconquista@AdminisadorsMBP.bbrouter>
2024-06-06 15:31:07 -0300

    docs: ✏️ added new composite Datepicker component

---
+++ packages/implementations/react/src/composite/Datepicker/src/datepicker.stories.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { argTypesConvert } from ".storybook/utils";
+import { Box } from "@rarui-react/box";
+
+import { Datepicker } from "./index";
+import docs from "./datepicker.docs.json";
+
+const meta: Meta<typeof Datepicker> = {
+  title: "Composite/Datepicker",
+  render: (args) => {
+    const [date, setDate] = useState(new Date());
+    return (
+      <Box display="flex" justifyContent="center">
+        <Datepicker {...args} selected={date} onChange={setDate as any} />
+      </Box>
+    );
+  },
+  component: Datepicker,
+  argTypes: argTypesConvert(docs),
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Datepicker>;
+
+export const basic: Story = {};
+
+export const yearPicker: Story = {
+  args: {
+    showYearPicker: true,
+    dateFormat: "yyyy",
+  },
+};
+
+export const monthPicker: Story = {
+  args: {
+    showYearPicker: true,
+    dateFormat: "MM/yyyy",
+  },
+};
+
+export const withTimePicker: Story = {
+  args: {
+    showTimeSelect: true,
+    dateFormat: "MMMM d, yyyy h:mm aa",
+  },
+};
+
+export const rangePicker: Story = {
+  render: (args) => {
+    const defaultStartDate = new Date("2024-08-01");
+    const defaultEndDate = new Date("2024-09-01");
+    const [startDate, setStartDate] = useState(defaultStartDate);
+    const [endDate, setEndDate] = useState(defaultEndDate);
+
+    const handleChange = ([newStartDate, newEndDate]: [Date, Date]) => {
+      setStartDate(newStartDate);
+      setEndDate(newEndDate);
+    };
+
+    return (
+      <Box display="flex" justifyContent="center">
+        <Datepicker
+          {...args}
+          selected={startDate}
+          startDate={startDate}
+          endDate={endDate}
+          onChange={handleChange as any}
+        />
+      </Box>
+    );
+  },
+  args: {
+    showTimeSelect: true,
+    dateFormat: "MMMM d, yyyy h:mm aa",
+  },
+};
