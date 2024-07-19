eaf5096d87471d2d08c40fa32eb1d99368204125
Junior Conquista <juniorconquista@Administradors-MacBook-Pro.local>
2024-07-19 15:40:08 -0300

    chore: publish release stable [skip ci]

--- .storybook/decorators/WithHTMLSource/WithHTMLSource.tsx
+++ .storybook/decorators/WithHTMLSource/WithHTMLSource.tsx
@@ -1,24 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { renderToStaticMarkup } from "react-dom/server";
 import { Prism } from "react-syntax-highlighter";
-import { useDarkMode } from "storybook-dark-mode";
+import { addons } from "@storybook/preview-api";
+import { DARK_MODE_EVENT_NAME, useDarkMode } from "storybook-dark-mode";
 import {
   prism as prismStyle,
   coldarkDark,
 } from "react-syntax-highlighter/dist/esm/styles/prism";
 import formatter from "code-formatter";
+import CopyToClipboard from "react-copy-to-clipboard";
 import tokens from "@rarui/tokens/dist/js/tokens";
 
 import { Tabs } from "@rarui-react/tabs";
 import { Box } from "@rarui-react/box";
 import { IconButton } from "@rarui-react/icon-button";
-import { FileCopyOutlinedIcon } from "@rarui/icons";
+import { FileCopyOutlinedIcon, CheckIcon } from "@rarui/icons";
 import { hashsRemoves } from "./withHTMLSource.definitions";
 import "./withHTMLSource.styles.css";
 
+const channel = addons.getChannel();
+
 export const WithHTMLSource = (StoryFn: any) => {
   const [selected, setSelected] = useState(0);
-  const isDark = useDarkMode();
+  const [hasCopied, setHasCopied] = useState(false);
+  const [isDark, setDark] = useState<boolean>(useDarkMode());
+
+  useEffect(() => {
+    if (hasCopied) setTimeout(() => setHasCopied(false), 1500);
+  }, [hasCopied]);
+
+  useEffect(() => {
+    channel.on(DARK_MODE_EVENT_NAME, setDark);
+    return () => channel.removeListener(DARK_MODE_EVENT_NAME, setDark);
+  }, [channel, setDark]);
+
   const html = renderToStaticMarkup(<StoryFn />);
   const formattedHtml = (formatter as any)(html, { method: "html" });
 
@@ -50,12 +65,17 @@ export const WithHTMLSource = (StoryFn: any) => {
               >
                 {hashsRemoves(formattedHtml)}
               </Prism>
-              <IconButton
-                source={<FileCopyOutlinedIcon />}
-                size="small"
-                variant="tonal"
-                appearance="neutral"
-              />
+              <CopyToClipboard
+                text={hashsRemoves(formattedHtml)}
+                onCopy={() => setHasCopied(true)}
+              >
+                <IconButton
+                  source={!hasCopied ? <FileCopyOutlinedIcon /> : <CheckIcon />}
+                  size="small"
+                  variant="tonal"
+                  appearance="neutral"
+                />
+              </CopyToClipboard>
             </Box>
           </Box>
         </Tabs.Item>
