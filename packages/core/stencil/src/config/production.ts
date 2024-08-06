2f5910debca26abf8497f526c1239aa02f0d5304
Junior Conquista <juniorconquista@AdminisadorsMBP.bbrouter>
2024-08-06 11:51:37 -0300

    feat: 🎸 create new package for build web components

--- packages/core/stencil/src/config/production.ts
+++ packages/core/stencil/src/config/production.ts
@@ -1,19 +1,30 @@
 /**
  * Created by: Júnior Conquista (junior@rarolabs.com.br)
  */
+
 import { Config } from "@stencil/core";
+import { aliasPlugin } from "../plugins";
 
 export const config: Config = {
   outputTargets: [
     {
       type: "dist",
     },
-    {
-      type: "docs-readme",
-    },
   ],
+
   testing: {
     browserHeadless: "new",
+  },
+  rollupPlugins: {
+    before: [aliasPlugin],
+  },
+  rollupConfig: {
+    inputOptions: {
+      external: (id) => {
+        // Externalize @rarui/styles and other external libraries
+        return /^@rarui\/styles/.test(id);
+      },
+    },
   },
 };
 
